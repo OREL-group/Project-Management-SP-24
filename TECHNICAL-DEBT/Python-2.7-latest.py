bcf87ab9ff5bce54ea53885c6d860d556e2dac6e
Orthogonal Research and Education Laboratory <38323286+Orthogonal-Research-Lab@users.noreply.github.com>
2024-03-10 11:54:08 -0500

    Update Python-2.7-latest.py

--- TECHNICAL-DEBT/Python-2.7-latest.py
+++ TECHNICAL-DEBT/Python-2.7-latest.py
@@ -1,1 +1,5 @@
+-------------------------------------------------------------------
+----  Please **don't update** to the latest version of Python  ----
+-------------------------------------------------------------------
 
+Use 4 billion **if** statement, if you dare [link](https://andreasjhkarlsson.github.io/jekyll/update/2023/12/27/4-billion-if-statements.html)
